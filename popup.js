eae5f59c0229b60669e332a29f6ccb5461948e6b
rywaroy <464485682@qq.com>
2021-11-15 11:22:03 +0800

    feat: popup 交互

--- popup.js
+++ popup.js
@@ -13,7 +13,60 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         );
     }
-    
-    sendMessageToContent({ type: 'uifriend-start' });
 
-})
+    class State {
+        constructor() {
+            this.state = 0;
+            this.button = document.querySelector('.popup-container-button');
+            this.bind();
+        }
+
+        bind() {
+            this.button.addEventListener('click', () => {
+                if (this.state === 0) {
+                    sendMessageToContent({ type: 'start' }, res => {
+                        if (res === 1) {
+                            this.start();
+                        }
+                    })
+                }
+                if (this.state === 1) {
+                    sendMessageToContent({ type: 'stop' }, res => {
+                        if (res === 0) {
+                            this.stop();
+                        }
+                    })
+                }
+            });
+        }
+
+        start() {
+            this.button.classList.remove('start');
+            this.button.classList.add('stop');
+            this.button.innerHTML = '关闭';
+            this.state = 1;
+        }
+
+        stop() {
+            this.button.classList.remove('stop');
+            this.button.classList.add('start');
+            this.button.innerHTML = '开启';
+            this.state = 0;
+        }
+
+        getState() {
+            sendMessageToContent({ type: 'state' }, res => {
+                if (res === 0) {
+                    this.stop();
+                }
+                if (res === 1) {
+                    this.start();
+                }
+            });
+        }
+    }
+
+    const state = new State();
+
+    state.getState();
+});
